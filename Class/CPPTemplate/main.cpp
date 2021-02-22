18b6d8fcf293cdd08865c652bc8a393b4e519f8e
Mark Lehr <mark.lehr@rcc.edu>
2021-02-22 08:45:43 -0800

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on February 19, 2020, 11:51 AM
+ * Purpose:  C++ Template
+ */
+
+//System Level Libraries
+#include <iostream>   //I/O Library
+#include <cmath>      //Math Library
+using namespace std;  //Library Scope
+
+//User Libraries
+
+//Global Constants
+//Science and Math, Conversions, Higher Dimensions
+//const to follow
+
+//Function Prototypes
+
+//Execution Starts Here
+int main(int argc, char** argv){
+    //Set Random Number Seed Here
+    
+    //Declare Variables - Known and Unknown, units, range, description
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> i.e. process the inputs
+    cout<<log(9)<<endl;
+    
+    cout<<exp(2.19722)<<endl;
+    
+    //Display the outputs
+    
+    //Clean up - File closing, memory deallocation, etc....
+
+    //Exit Stage Right!
+    return 0;
+}
+
+//Function Implementations
